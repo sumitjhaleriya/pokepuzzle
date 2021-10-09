49a57b3ad3949d697a5601d0145274fe308f2dd4
Sumit Jhaleriya <rkjhaleria75@gmail.com>
2021-10-09 22:32:40 +0530

    first one

---
+++ cfg.py
@@ -0,0 +1,18 @@
+import os
+
+SCREENSIZE = (700, 700)
+
+IMGSIZE = (300, 300)
+
+PICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'resources/pictures')
+
+FONTPATH = os.path.join(os.getcwd(), 'resources/font/FZSTK.TTF')
+
+BACKGROUNDCOLOR = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+
+RANDNUM = 100
